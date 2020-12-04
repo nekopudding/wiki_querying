cf54834bfa9d420456f544c9520c0c5cb977ff90
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 22:06:43 -0800

    added specs and RI and AF and more tests.

--- src/test/java/cpen221/mp3/Tests.java
+++ src/test/java/cpen221/mp3/Tests.java
@@ -4,8 +4,9 @@ import cpen221.mp3.fsftbuffer.*;
 import org.junit.Test;
 
 import java.io.InvalidObjectException;
+import java.nio.Buffer;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class Tests {
 
@@ -50,6 +51,114 @@ public class Tests {
             System.out.println("Could not find 'a'.");
             assertEquals(0,1);
         }
+        try {
+            Thread.sleep(10000);
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread interrupted.");
+        }
+        try {
+            assertEquals(buffer.get("a"), null);
+        }
+        catch (InvalidObjectException e){
+            System.out.println("Could not find 'a'. (Correct)");
+            assertEquals(1,1);
+        }
+
+    }
+
+    @Test
+    public void testCapacity(){
+        Bufferable a = new Bufferable() {
+            @Override
+            public String id() {
+                return "a";
+            }
+        };
+        Bufferable b = new Bufferable() {
+            @Override
+            public String id() {
+                return "b";
+            }
+        };
+        Bufferable c = new Bufferable(){
+            @Override
+            public String id() {
+                return "c";
+            }
+        };
+        Bufferable d = new Bufferable() {
+            @Override
+            public String id() {
+                return "d";
+            }
+        };
+        FSFTBuffer<Bufferable> buffer = new FSFTBuffer<>(3, 8);
+        buffer.put(a);
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread interrupted.");
+        }
+        buffer.put(b);
+        buffer.put(c);
+        buffer.put(d);
+        try {
+            assertEquals(buffer.get("a"), null);
+        }
+        catch (InvalidObjectException e){
+            System.out.println("Could not find 'a'. (Correct)");
+            assertEquals(1,1);
+        }
+        try {
+            assertEquals(buffer.get("b"), b);
+        }
+        catch (InvalidObjectException e){
+            System.out.println("Could not find 'b'.");
+            assertEquals(0,1);
+        }
+        try {
+            assertEquals(buffer.get("c"), c);
+        }
+        catch (InvalidObjectException e){
+            System.out.println("Could not find 'c'.");
+            assertEquals(0,1);
+        }
+        try {
+            assertEquals(buffer.get("d"), d);
+        }
+        catch (InvalidObjectException e){
+            System.out.println("Could not find 'd'.");
+            assertEquals(0,1);
+        }
+
+    }
+
+    @Test
+    public void test2(){
+        Bufferable a = new Bufferable() {
+            @Override
+            public String id() {
+                return "a";
+            }
+        };
+        FSFTBuffer<Bufferable> buffer = new FSFTBuffer<>(5, 8);
+        buffer.put(a);
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread interrupted.");
+        }
+        assertTrue(buffer.touch("a"));
+        try {
+            Thread.sleep(6000);
+        }
+        catch (InterruptedException e){
+            System.out.println("Thread interrupted.");
+        }
+        assertTrue(buffer.update(a));
         try {
             Thread.sleep(10000);
         }
