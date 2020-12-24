ac1584da531a405c98989a239ae3518335f8a063
t-liao <ter92240@gmail.com>
2020-12-23 17:17:16 -0800

    timedout test case test case

--- src/test/java/cpen221/mp3/Tests_Task5.java
+++ src/test/java/cpen221/mp3/Tests_Task5.java
@@ -1,16 +1,26 @@
 package cpen221.mp3;
 
 import cpen221.mp3.wikimediator.WikiMediator;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import java.util.List;
 
 public class Tests_Task5 {
 
+    private static int TIMEOUT = 10;
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(TIMEOUT);
+
     @Test
     public void testString() {
 
+
         List<String> w = WikiMediator.executeQuery("get category where (author is 'CLCStudent' and (title is 'Barack Obama' or title is 'Naomi Klein'))");
+
+
 
     }
 
